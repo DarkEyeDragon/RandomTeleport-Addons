777a5de56d8a6c07ebb5877804b71eddbef5f670
DarkEyeDragon <mccrafter4liver@gmail.com>
2021-12-28 18:30:30 +0100

    Add min required version

--- KingdomsXValidator/src/main/java/me/darkeyedragon/randomtp/addon/KingdomsXValidator.java
+++ KingdomsXValidator/src/main/java/me/darkeyedragon/randomtp/addon/KingdomsXValidator.java
@@ -2,11 +2,16 @@ package me.darkeyedragon.randomtp.addon;
 
 import me.darkeyedragon.randomtp.api.addon.RandomAddon;
 import me.darkeyedragon.randomtp.api.world.location.RandomLocation;
+import me.darkeyedragon.randomtp.common.addon.BaseRequiredPlugin;
 import me.darkeyedragon.randomtp.util.WorldUtil;
 import org.kingdoms.constants.land.Land;
 
 public class KingdomsXValidator extends RandomAddon {
 
+
+    public KingdomsXValidator() {
+        super.addRequiredPlugin(new BaseRequiredPlugin("Kingdoms", "1.11.11"));
+    }
 
     @Override
     public String getIdentifier() {
